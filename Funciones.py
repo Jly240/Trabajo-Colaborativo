92b89116dcc4b6c722012ab740fc20496fb3489b
Jly240 <julrodriguezar@unal.edu.co>
2025-04-23 12:35:56 -0500

    Primera actividad

---
+++ Funciones.py
@@ -0,0 +1,15 @@
+#Definicion de un potencia
+def xCuadrado(x,y):
+    z = x**y
+    print(f"este es el resultado de la funcion",{z})
+    return z
+xCuadrado(7,4)    
+
+
+#Area de un circulo
+
+def AreaDelCirculo(R):
+    x = 3.1416*(R)**2
+    print("El area del circulo es", {x})
+    return R
+AreaDelCirculo(20)
